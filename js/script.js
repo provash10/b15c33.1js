a909dfb5104471c2f98047961ba6ee0c8e5a52ac
provash10 <provash20cb@gmail.com>
2024-11-26 01:51:47 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+function sum(){
+    let x = 10;
+    let y = 40;
+    let result = x+y;
+    console.log(result);
+}
+sum();
+
+// ==========
+
+// function sum(number1, number2){
+//     let x = number1;
+//     let y = number2;
+//     let result = x+y;
+//     console.log(result);
+// }
+// // sum();
+
+// sum(80, 120);
+// sum(90, 10);
+
+
+// ====================
+function sum(number1, number2){
+    let x = number1;
+    let y = number2;
+    let result = x+y;
+    // console.log(result);
+    return result;
+}
+// sum();
+
+// sum(80, 120);
+// sum(90, 10);
+
+// console.log(sum(80, 120));
+
+// OR 
+let summation = (80, 220);
+
+console.log(summation);
+
+let secondSum = summation + 120
+console.log(secondSum);
+
+let thirdSum = sum (30, 50)
+console.log(thirdSum);
+
+// For Subtraction
+
+function sub(number3, number4){
+    let x = number3;
+    let y = number4;
+    let result = number3 - number4;
+      return result;
+}
+// sub (120,90);
+let subtraction = sub (120,90);
+console.log(subtraction);
